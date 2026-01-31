7b5fadceed428d4d7c8ef46d4334d4759c926297
p0675892-jpg <p0675892@gmail.com>
2026-01-30 19:48:03 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,20 +5,20 @@ import Dashboard from "./pages/Dashboard";
 import EditProduct from "./pages/EditProduct";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
+import PrivateRoute from "./components/PrivateRoute";
 
-function App() {
+export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<StoreFront />} />
-        <Route path="/add" element={<AddProduct />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/edit/:id" element={<EditProduct />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
+
+        <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+        <Route path="/add" element={<PrivateRoute><AddProduct /></PrivateRoute>} />
+        <Route path="/edit/:id" element={<PrivateRoute><EditProduct /></PrivateRoute>} />
       </Routes>
     </BrowserRouter>
   );
 }
-
-export default App;
