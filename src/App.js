23a0f63661849de0c6371bfd607e26e8b1463de6
p0675892-jpg <p0675892@gmail.com>
2026-01-30 18:22:24 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import StoreFront from "./pages/StoreFront";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 import AddProduct from "./pages/AddProduct";
+import Dashboard from "./pages/Dashboard";
+import { auth } from "./firebase";
 
 function App() {
+  const user = auth.currentUser;
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<StoreFront />} />
-        <Route path="/add" element={<AddProduct />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+
+        {/* Protected */}
+        <Route
+          path="/dashboard"
+          element={user ? <Dashboard /> : <Login />}
+        />
+        <Route
+          path="/add"
+          element={user ? <AddProduct /> : <Login />}
+        />
       </Routes>
     </Router>
   );
